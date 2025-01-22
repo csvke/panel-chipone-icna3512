877a86bf7521388ab6f87009dcb56a62590121c8
csvke <frankie.yuen@me.com>
2025-01-22 18:30:15 +0800

    PIP-47 change the naming from jdi_panel and jdi to icna3512_panel and icna3512

--- panel-chipone-icna3512.c
+++ panel-chipone-icna3512.c
@@ -1,16 +1,3 @@
-// SPDX-License-Identifier: GPL-2.0-only
-/*
- * Copyright (C) 2016 InforceComputing
- * Author: Vinay Simha BN <simhavcs@gmail.com>
- *
- * Copyright (C) 2016 Linaro Ltd
- * Author: Sumit Semwal <sumit.semwal@linaro.org>
- *
- * From internet archives, the panel for Nexus 7 2nd Gen, 2013 model is a
- * JDI model LT070ME05000, and its data sheet is at:
- * http://panelone.net/en/7-0-inch/JDI_LT070ME05000_7.0_inch-datasheet
- */
-
 #include <linux/backlight.h>
 #include <linux/delay.h>
 #include <linux/gpio/consumer.h>
@@ -30,7 +17,7 @@ static const char * const regulator_names[] = {
 	"iovcc"
 };
 
-struct jdi_panel {
+struct icna3512_panel {
 	struct drm_panel base;
 	struct mipi_dsi_device *dsi;
 
@@ -47,15 +34,15 @@ struct jdi_panel {
 	const struct drm_display_mode *mode;
 };
 
-static inline struct jdi_panel *to_jdi_panel(struct drm_panel *panel)
+static inline struct icna3512_panel *to_icna3512_panel(struct drm_panel *panel)
 {
-	return container_of(panel, struct jdi_panel, base);
+	return container_of(panel, struct icna3512_panel, base);
 }
 
-static int jdi_panel_init(struct jdi_panel *jdi)
+static int icna3512_panel_init(struct icna3512_panel *icna3512)
 {
-	struct mipi_dsi_device *dsi = jdi->dsi;
-	struct device *dev = &jdi->dsi->dev;
+	struct mipi_dsi_device *dsi = icna3512->dsi;
+	struct device *dev = &icna3512->dsi->dev;
 	int ret;
 
 	dsi->mode_flags |= MIPI_DSI_MODE_LPM;
@@ -72,13 +59,13 @@ static int jdi_panel_init(struct jdi_panel *jdi)
 		return ret;
 	}
 
-	ret = mipi_dsi_dcs_set_column_address(dsi, 0, jdi->mode->hdisplay - 1);
+	ret = mipi_dsi_dcs_set_column_address(dsi, 0, icna3512->mode->hdisplay - 1);
 	if (ret < 0) {
 		dev_err(dev, "failed to set column address: %d\n", ret);
 		return ret;
 	}
 
-	ret = mipi_dsi_dcs_set_page_address(dsi, 0, jdi->mode->vdisplay - 1);
+	ret = mipi_dsi_dcs_set_page_address(dsi, 0, icna3512->mode->vdisplay - 1);
 	if (ret < 0) {
 		dev_err(dev, "failed to set page address: %d\n", ret);
 		return ret;
@@ -142,10 +129,10 @@ static int jdi_panel_init(struct jdi_panel *jdi)
 	return 0;
 }
 
-static int jdi_panel_on(struct jdi_panel *jdi)
+static int icna3512_panel_on(struct icna3512_panel *icna3512)
 {
-	struct mipi_dsi_device *dsi = jdi->dsi;
-	struct device *dev = &jdi->dsi->dev;
+	struct mipi_dsi_device *dsi = icna3512->dsi;
+	struct device *dev = &icna3512->dsi->dev;
 	int ret;
 
 	dsi->mode_flags |= MIPI_DSI_MODE_LPM;
@@ -157,10 +144,10 @@ static int jdi_panel_on(struct jdi_panel *jdi)
 	return ret;
 }
 
-static void jdi_panel_off(struct jdi_panel *jdi)
+static void icna3512_panel_off(struct icna3512_panel *icna3512)
 {
-	struct mipi_dsi_device *dsi = jdi->dsi;
-	struct device *dev = &jdi->dsi->dev;
+	struct mipi_dsi_device *dsi = icna3512->dsi;
+	struct device *dev = &icna3512->dsi->dev;
 	int ret;
 
 	dsi->mode_flags &= ~MIPI_DSI_MODE_LPM;
@@ -176,56 +163,56 @@ static void jdi_panel_off(struct jdi_panel *jdi)
 	msleep(100);
 }
 
-static int jdi_panel_disable(struct drm_panel *panel)
+static int icna3512_panel_disable(struct drm_panel *panel)
 {
-	struct jdi_panel *jdi = to_jdi_panel(panel);
+	struct icna3512_panel *icna3512 = to_icna3512_panel(panel);
 
-	if (!jdi->enabled)
+	if (!icna3512->enabled)
 		return 0;
 
-	backlight_disable(jdi->backlight);
+	backlight_disable(icna3512->backlight);
 
-	jdi->enabled = false;
+	icna3512->enabled = false;
 
 	return 0;
 }
 
-static int jdi_panel_unprepare(struct drm_panel *panel)
+static int icna3512_panel_unprepare(struct drm_panel *panel)
 {
-	struct jdi_panel *jdi = to_jdi_panel(panel);
-	struct device *dev = &jdi->dsi->dev;
+	struct icna3512_panel *icna3512 = to_icna3512_panel(panel);
+	struct device *dev = &icna3512->dsi->dev;
 	int ret;
 
-	if (!jdi->prepared)
+	if (!icna3512->prepared)
 		return 0;
 
-	jdi_panel_off(jdi);
+	icna3512_panel_off(icna3512);
 
-	ret = regulator_bulk_disable(ARRAY_SIZE(jdi->supplies), jdi->supplies);
+	ret = regulator_bulk_disable(ARRAY_SIZE(icna3512->supplies), icna3512->supplies);
 	if (ret < 0)
 		dev_err(dev, "regulator disable failed, %d\n", ret);
 
-	gpiod_set_value_cansleep(jdi->enable_gpio, 0);
+	gpiod_set_value_cansleep(icna3512->enable_gpio, 0);
 
-	gpiod_set_value_cansleep(jdi->reset_gpio, 1);
+	gpiod_set_value_cansleep(icna3512->reset_gpio, 1);
 
-	gpiod_set_value_cansleep(jdi->dcdc_en_gpio, 0);
+	gpiod_set_value_cansleep(icna3512->dcdc_en_gpio, 0);
 
-	jdi->prepared = false;
+	icna3512->prepared = false;
 
 	return 0;
 }
 
-static int jdi_panel_prepare(struct drm_panel *panel)
+static int icna3512_panel_prepare(struct drm_panel *panel)
 {
-	struct jdi_panel *jdi = to_jdi_panel(panel);
-	struct device *dev = &jdi->dsi->dev;
+	struct icna3512_panel *icna3512 = to_icna3512_panel(panel);
+	struct device *dev = &icna3512->dsi->dev;
 	int ret;
 
-	if (jdi->prepared)
+	if (icna3512->prepared)
 		return 0;
 
-	ret = regulator_bulk_enable(ARRAY_SIZE(jdi->supplies), jdi->supplies);
+	ret = regulator_bulk_enable(ARRAY_SIZE(icna3512->supplies), icna3512->supplies);
 	if (ret < 0) {
 		dev_err(dev, "regulator enable failed, %d\n", ret);
 		return ret;
@@ -233,55 +220,55 @@ static int jdi_panel_prepare(struct drm_panel *panel)
 
 	msleep(20);
 
-	gpiod_set_value_cansleep(jdi->dcdc_en_gpio, 1);
+	gpiod_set_value_cansleep(icna3512->dcdc_en_gpio, 1);
 	usleep_range(10, 20);
 
-	gpiod_set_value_cansleep(jdi->reset_gpio, 0);
+	gpiod_set_value_cansleep(icna3512->reset_gpio, 0);
 	usleep_range(10, 20);
 
-	gpiod_set_value_cansleep(jdi->enable_gpio, 1);
+	gpiod_set_value_cansleep(icna3512->enable_gpio, 1);
 	usleep_range(10, 20);
 
-	ret = jdi_panel_init(jdi);
+	ret = icna3512_panel_init(icna3512);
 	if (ret < 0) {
 		dev_err(dev, "failed to init panel: %d\n", ret);
 		goto poweroff;
 	}
 
-	ret = jdi_panel_on(jdi);
+	ret = icna3512_panel_on(icna3512);
 	if (ret < 0) {
 		dev_err(dev, "failed to set panel on: %d\n", ret);
 		goto poweroff;
 	}
 
-	jdi->prepared = true;
+	icna3512->prepared = true;
 
 	return 0;
 
 poweroff:
-	ret = regulator_bulk_disable(ARRAY_SIZE(jdi->supplies), jdi->supplies);
+	ret = regulator_bulk_disable(ARRAY_SIZE(icna3512->supplies), icna3512->supplies);
 	if (ret < 0)
 		dev_err(dev, "regulator disable failed, %d\n", ret);
 
-	gpiod_set_value_cansleep(jdi->enable_gpio, 0);
+	gpiod_set_value_cansleep(icna3512->enable_gpio, 0);
 
-	gpiod_set_value_cansleep(jdi->reset_gpio, 1);
+	gpiod_set_value_cansleep(icna3512->reset_gpio, 1);
 
-	gpiod_set_value_cansleep(jdi->dcdc_en_gpio, 0);
+	gpiod_set_value_cansleep(icna3512->dcdc_en_gpio, 0);
 
 	return ret;
 }
 
-static int jdi_panel_enable(struct drm_panel *panel)
+static int icna3512_panel_enable(struct drm_panel *panel)
 {
-	struct jdi_panel *jdi = to_jdi_panel(panel);
+	struct icna3512_panel *icna3512 = to_icna3512_panel(panel);
 
-	if (jdi->enabled)
+	if (icna3512->enabled)
 		return 0;
 
-	backlight_enable(jdi->backlight);
+	backlight_enable(icna3512->backlight);
 
-	jdi->enabled = true;
+	icna3512->enabled = true;
 
 	return 0;
 }
@@ -299,12 +286,12 @@ static const struct drm_display_mode default_mode = {
 		.flags = 0,
 };
 
-static int jdi_panel_get_modes(struct drm_panel *panel,
+static int icna3512_panel_get_modes(struct drm_panel *panel,
 			       struct drm_connector *connector)
 {
 	struct drm_display_mode *mode;
-	struct jdi_panel *jdi = to_jdi_panel(panel);
-	struct device *dev = &jdi->dsi->dev;
+	struct icna3512_panel *icna3512 = to_icna3512_panel(panel);
+	struct device *dev = &icna3512->dsi->dev;
 
 	mode = drm_mode_duplicate(connector->dev, &default_mode);
 	if (!mode) {
@@ -377,76 +364,76 @@ drm_panel_create_dsi_backlight(struct mipi_dsi_device *dsi)
 					      &dsi_bl_ops, &props);
 }
 
-static const struct drm_panel_funcs jdi_panel_funcs = {
-	.disable = jdi_panel_disable,
-	.unprepare = jdi_panel_unprepare,
-	.prepare = jdi_panel_prepare,
-	.enable = jdi_panel_enable,
-	.get_modes = jdi_panel_get_modes,
+static const struct drm_panel_funcs icna3512_panel_funcs = {
+	.disable = icna3512_panel_disable,
+	.unprepare = icna3512_panel_unprepare,
+	.prepare = icna3512_panel_prepare,
+	.enable = icna3512_panel_enable,
+	.get_modes = icna3512_panel_get_modes,
 };
 
-static const struct of_device_id jdi_of_match[] = {
-	{ .compatible = "jdi,lt070me05000", },
+static const struct of_device_id icna3512_of_match[] = {
+	{ .compatible = "icna3512,lt070me05000", },
 	{ }
 };
-MODULE_DEVICE_TABLE(of, jdi_of_match);
+MODULE_DEVICE_TABLE(of, icna3512_of_match);
 
-static int jdi_panel_add(struct jdi_panel *jdi)
+static int icna3512_panel_add(struct icna3512_panel *icna3512)
 {
-	struct device *dev = &jdi->dsi->dev;
+	struct device *dev = &icna3512->dsi->dev;
 	int ret;
 	unsigned int i;
 
-	jdi->mode = &default_mode;
+	icna3512->mode = &default_mode;
 
-	for (i = 0; i < ARRAY_SIZE(jdi->supplies); i++)
-		jdi->supplies[i].supply = regulator_names[i];
+	for (i = 0; i < ARRAY_SIZE(icna3512->supplies); i++)
+		icna3512->supplies[i].supply = regulator_names[i];
 
-	ret = devm_regulator_bulk_get(dev, ARRAY_SIZE(jdi->supplies),
-				      jdi->supplies);
+	ret = devm_regulator_bulk_get(dev, ARRAY_SIZE(icna3512->supplies),
+				      icna3512->supplies);
 	if (ret < 0)
 		return dev_err_probe(dev, ret,
 				     "failed to init regulator, ret=%d\n", ret);
 
-	jdi->enable_gpio = devm_gpiod_get(dev, "enable", GPIOD_OUT_LOW);
-	if (IS_ERR(jdi->enable_gpio)) {
-		return dev_err_probe(dev, PTR_ERR(jdi->enable_gpio),
+	icna3512->enable_gpio = devm_gpiod_get(dev, "enable", GPIOD_OUT_LOW);
+	if (IS_ERR(icna3512->enable_gpio)) {
+		return dev_err_probe(dev, PTR_ERR(icna3512->enable_gpio),
 				     "cannot get enable-gpio %d\n", ret);
 	}
 
-	jdi->reset_gpio = devm_gpiod_get(dev, "reset", GPIOD_OUT_HIGH);
-	if (IS_ERR(jdi->reset_gpio))
-		return dev_err_probe(dev, PTR_ERR(jdi->reset_gpio),
+	icna3512->reset_gpio = devm_gpiod_get(dev, "reset", GPIOD_OUT_HIGH);
+	if (IS_ERR(icna3512->reset_gpio))
+		return dev_err_probe(dev, PTR_ERR(icna3512->reset_gpio),
 				     "cannot get reset-gpios %d\n", ret);
 
-	jdi->dcdc_en_gpio = devm_gpiod_get(dev, "dcdc-en", GPIOD_OUT_LOW);
-	if (IS_ERR(jdi->dcdc_en_gpio))
-		return dev_err_probe(dev, PTR_ERR(jdi->dcdc_en_gpio),
+	icna3512->dcdc_en_gpio = devm_gpiod_get(dev, "dcdc-en", GPIOD_OUT_LOW);
+	if (IS_ERR(icna3512->dcdc_en_gpio))
+		return dev_err_probe(dev, PTR_ERR(icna3512->dcdc_en_gpio),
 				     "cannot get dcdc-en-gpio %d\n", ret);
 
-	jdi->backlight = drm_panel_create_dsi_backlight(jdi->dsi);
-	if (IS_ERR(jdi->backlight))
-		return dev_err_probe(dev, PTR_ERR(jdi->backlight),
+	icna3512->backlight = drm_panel_create_dsi_backlight(icna3512->dsi);
+	if (IS_ERR(icna3512->backlight))
+		return dev_err_probe(dev, PTR_ERR(icna3512->backlight),
 				     "failed to register backlight %d\n", ret);
 
-	jdi->base.prepare_prev_first = true;
-	drm_panel_init(&jdi->base, &jdi->dsi->dev, &jdi_panel_funcs,
+	icna3512->base.prepare_prev_first = true;
+	drm_panel_init(&icna3512->base, &icna3512->dsi->dev, &icna3512_panel_funcs,
 		       DRM_MODE_CONNECTOR_DSI);
 
-	drm_panel_add(&jdi->base);
+	drm_panel_add(&icna3512->base);
 
 	return 0;
 }
 
-static void jdi_panel_del(struct jdi_panel *jdi)
+static void icna3512_panel_del(struct icna3512_panel *icna3512)
 {
-	if (jdi->base.dev)
-		drm_panel_remove(&jdi->base);
+	if (icna3512->base.dev)
+		drm_panel_remove(&icna3512->base);
 }
 
-static int jdi_panel_probe(struct mipi_dsi_device *dsi)
+static int icna3512_panel_probe(struct mipi_dsi_device *dsi)
 {
-	struct jdi_panel *jdi;
+	struct icna3512_panel *icna3512;
 	int ret;
 
 	dsi->lanes = 4;
@@ -454,33 +441,33 @@ static int jdi_panel_probe(struct mipi_dsi_device *dsi)
 	dsi->mode_flags =  MIPI_DSI_MODE_VIDEO_HSE | MIPI_DSI_MODE_VIDEO |
 			   MIPI_DSI_CLOCK_NON_CONTINUOUS;
 
-	jdi = devm_kzalloc(&dsi->dev, sizeof(*jdi), GFP_KERNEL);
-	if (!jdi)
+	icna3512 = devm_kzalloc(&dsi->dev, sizeof(*icna3512), GFP_KERNEL);
+	if (!icna3512)
 		return -ENOMEM;
 
-	mipi_dsi_set_drvdata(dsi, jdi);
+	mipi_dsi_set_drvdata(dsi, icna3512);
 
-	jdi->dsi = dsi;
+	icna3512->dsi = dsi;
 
-	ret = jdi_panel_add(jdi);
+	ret = icna3512_panel_add(icna3512);
 	if (ret < 0)
 		return ret;
 
 	ret = mipi_dsi_attach(dsi);
 	if (ret < 0) {
-		jdi_panel_del(jdi);
+		icna3512_panel_del(icna3512);
 		return ret;
 	}
 
 	return 0;
 }
 
-static void jdi_panel_remove(struct mipi_dsi_device *dsi)
+static void icna3512_panel_remove(struct mipi_dsi_device *dsi)
 {
-	struct jdi_panel *jdi = mipi_dsi_get_drvdata(dsi);
+	struct icna3512_panel *icna3512 = mipi_dsi_get_drvdata(dsi);
 	int ret;
 
-	ret = jdi_panel_disable(&jdi->base);
+	ret = icna3512_panel_disable(&icna3512->base);
 	if (ret < 0)
 		dev_err(&dsi->dev, "failed to disable panel: %d\n", ret);
 
@@ -489,28 +476,28 @@ static void jdi_panel_remove(struct mipi_dsi_device *dsi)
 		dev_err(&dsi->dev, "failed to detach from DSI host: %d\n",
 			ret);
 
-	jdi_panel_del(jdi);
+	icna3512_panel_del(icna3512);
 }
 
-static void jdi_panel_shutdown(struct mipi_dsi_device *dsi)
+static void icna3512_panel_shutdown(struct mipi_dsi_device *dsi)
 {
-	struct jdi_panel *jdi = mipi_dsi_get_drvdata(dsi);
+	struct icna3512_panel *icna3512 = mipi_dsi_get_drvdata(dsi);
 
-	jdi_panel_disable(&jdi->base);
+	icna3512_panel_disable(&icna3512->base);
 }
 
-static struct mipi_dsi_driver jdi_panel_driver = {
+static struct mipi_dsi_driver icna3512_panel_driver = {
 	.driver = {
-		.name = "panel-jdi-lt070me05000",
-		.of_match_table = jdi_of_match,
+		.name = "panel-icna3512-lt070me05000",
+		.of_match_table = icna3512_of_match,
 	},
-	.probe = jdi_panel_probe,
-	.remove = jdi_panel_remove,
-	.shutdown = jdi_panel_shutdown,
+	.probe = icna3512_panel_probe,
+	.remove = icna3512_panel_remove,
+	.shutdown = icna3512_panel_shutdown,
 };
-module_mipi_dsi_driver(jdi_panel_driver);
+module_mipi_dsi_driver(icna3512_panel_driver);
 
 MODULE_AUTHOR("Sumit Semwal <sumit.semwal@linaro.org>");
 MODULE_AUTHOR("Vinay Simha BN <simhavcs@gmail.com>");
-MODULE_DESCRIPTION("JDI LT070ME05000 WUXGA");
+MODULE_DESCRIPTION("icna3512 LT070ME05000 WUXGA");
 MODULE_LICENSE("GPL v2");
